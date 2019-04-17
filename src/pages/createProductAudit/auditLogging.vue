1c2558ca68c66cb84f6d0b2ae0935e1a111a0939
guohuiming <mthgh0618@163.com>
2019-04-17 11:29:52 +0800

    审核结果状态变更

--- src/pages/createProductAudit/auditLogging.vue
+++ src/pages/createProductAudit/auditLogging.vue
@@ -114,8 +114,8 @@
         },
         statusList: [
           {label: '全部', value: ''},
-          {label: '通过', value: 0},
-          {label: '不通过', value: 1},
+          {label: '审核不通过', value: 0},
+          {label: '审核通过', value: 1},
         ],
         sourceList: [
           {label: '全部', value: ''},
